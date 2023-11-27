ec4f8f5443a6d1950a8459cb562553866b1db410
TostoaKula <125472037+TostoaKula@users.noreply.github.com>
2023-11-27 12:09:44 +0100

    update modellen

--- backend/models/rocket.js
+++ backend/models/rocket.js
@@ -1,14 +1,13 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-let rocketSchema = new Schema(
-    {
-        _id:            {type: Number},
-        LaunchId:       {type: Number},
-        failed:         {type: Boolean},
-        topheight:      {type: Number},
-        topspeed:       {type: Number}
-    }
-);
+let rocketSchema = new Schema({
+  _id: { type: Number },
+  LaunchId: { type: Number },
+  failed: { type: Boolean },
+  topheight: { type: Number },
+  topspeed: { type: Number },
+  temperature: { type: Number },
+});
 
-module.exports = mongoose.model("rocket",rocketSchema);
+module.exports = mongoose.model("rocket", rocketSchema);
