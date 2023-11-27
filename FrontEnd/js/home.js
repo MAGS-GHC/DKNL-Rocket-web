59f1b8afa9fffc57a0e2cf3c6c9ee2a5ebc3bce1
cbc1000 <127729826+cbc1000@users.noreply.github.com>
2023-11-27 12:44:41 +0100

    homepage in progress

---
+++ FrontEnd/js/home.js
@@ -0,0 +1,42 @@
+async function rocketData() {
+    const grid = document.getElementById("container");
+    try {
+        const response = await fetch("https://dknl.onrender.com/api/rocket");
+        const userData = await response.json();
+        console.log(userData);
+        grid.innerHTML = `        
+        <div class="grid-container">
+            <div class="pfAngle">
+                <label class="infoLabel">
+                <img src="../assets/SVG/info.svg">
+                <div class="textContainer">
+                    <p id="launchBool">SUCCESS</p>
+                    <p >The Launching platform is at a perfect angle, Get ready to take flight!</p>
+                </div>
+                </label>
+            </div>   
+        <div class="Launch">
+            <label class="infoLabel2">You are clear for lift off</label>
+            <button class="launchBTN">Launch</button>
+        </div>
+        <div class="sensorInfo">
+            <p id="sensorName">Launch ID</p>
+            <p id="sensorData">${userData[0].LaunchId}</p>
+            <p id="sensorName">Temperature</p>
+            <p id="sensorData">${userData[0].temperature}</p>
+            <p id="sensorName">Top speed</p>
+            <p id="sensorData">${userData[0].topspeed}</p>
+            <p id="sensorName">Top height</p>
+            <p id="sensorData">${userData[0].topheight}</p>
+        </div>
+    </div>`;
+}
+    catch(error) {
+        console.log(error);
+    }
+}
+
+window.onload = function(){
+    rocketData();
+}
+    
