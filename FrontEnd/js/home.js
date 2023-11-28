adcb17866a3175a46a117c7ee8d74ebee0c55c3c
cbc1000 <127729826+cbc1000@users.noreply.github.com>
2023-11-28 10:59:04 +0100

    nav bar in progress

--- FrontEnd/js/home.js
+++ FrontEnd/js/home.js
@@ -1,17 +1,30 @@
 async function rocketData() {
+    const nav = document.getElementById('nav');
     const grid = document.getElementById("container");
     try {
         const response = await fetch("https://dknl.onrender.com/api/rocket");
         const userData = await response.json();
         console.log(userData);
-        grid.innerHTML = `        
+
+        nav.innerHTML = `
+        <div class="nav-container">
+            <div class="nav-wrapper">
+                <a href="../home/index.html" class="nav-link active">Home</a>
+                <a href="../catalogue/index.html" class="nav-link">Catalogue</a>
+                <a href="page3.html" class="nav-link">Page 3</a>
+                <a href="page4.html" class="nav-link">Page 4</a>
+                <a href="page5.html" class="nav-link">Page 5</a>
+            </div>
+        </div>`
+
+        grid.innerHTML = `       
         <div class="grid-container">
             <div class="pfAngle">
                 <label class="infoLabel">
                 <img src="../assets/SVG/info.svg">
                 <div class="textContainer">
                     <p id="launchBool">SUCCESS</p>
-                    <p>The Launching platform is at a perfect angle, Get ready to take flight!</p>
+                    <p id="text">The Launching platform is at a perfect angle, Get ready to take flight!</p>
                 </div>
                 </label>
             </div>   
