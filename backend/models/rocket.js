ff70a79f2f5ea1bb807455a877bc081d26f1b8f0
cbc1000 <127729826+cbc1000@users.noreply.github.com>
2023-11-29 08:48:46 +0100

    søren db

--- backend/models/rocket.js
+++ backend/models/rocket.js
@@ -1,6 +1,12 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+// Define the Counter schema
+const CounterSchema = mongoose.Schema({
+    _id: { type: String, required: true },
+    seq: { type: Number, default: 0 }
+});
+const Counter = mongoose.model('Counter', CounterSchema);
 
 let rocketSchema = new Schema({
 
@@ -13,4 +19,21 @@ let rocketSchema = new Schema({
 });
 
 
+// Function to get the next sequence
+function getNextSequence(name) {
+  var ret = Counter.findOneAndUpdate(
+      { _id: name },
+      { $inc: { seq: 1 } },
+      { new: true, upsert: true }
+  );
+  return ret.seq;
+}
+// Use the function before saving a new Rocket
+let newRocket = new Rocket({
+  _id: getNextSequence('rocketid'),
+  // other fields...
+});
+newRocket.save();
+
+const Rocket = mongoose.model('Rocket', RocketSchema);
 module.exports = mongoose.model("rocket", rocketSchema);
