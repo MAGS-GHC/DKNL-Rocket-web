f7e5d6ca1801513ecbc1a76f447c5ae959c36b44
TostoaKula <125472037+TostoaKula@users.noreply.github.com>
2023-11-29 08:58:02 +0100

    rocket update

--- backend/models/rocket.js
+++ backend/models/rocket.js
@@ -3,37 +3,39 @@ const Schema = mongoose.Schema;
 
 // Define the Counter schema
 const CounterSchema = mongoose.Schema({
-    _id: { type: String, required: true },
-    seq: { type: Number, default: 0 }
+  _id: { type: String, required: true },
+  seq: { type: Number, default: 0 },
 });
-const Counter = mongoose.model('Counter', CounterSchema);
+const Counter = mongoose.model("Counter", CounterSchema);
 
 let rocketSchema = new Schema({
+  _id: { type: Number },
 
-  _id:      { type: Number },
-  LaunchId: { type: Number },
-  failed:   { type: Boolean },
-  topheight:{ type: Number },
+  failed: { type: Boolean },
+  topheight: { type: Number },
   topspeed: { type: Number },
   temperature: { type: Number },
 });
 
-
 // Function to get the next sequence
 function getNextSequence(name) {
   var ret = Counter.findOneAndUpdate(
-      { _id: name },
-      { $inc: { seq: 1 } },
-      { new: true, upsert: true }
+    { _id: name },
+    { $inc: { seq: 1 } },
+    { new: true, upsert: true }
   );
   return ret.seq;
 }
 // Use the function before saving a new Rocket
 let newRocket = new Rocket({
-  _id: getNextSequence('rocketid'),
-  // other fields...
+  _id: getNextSequence("rocketid"),
+
+  failed: { type: Boolean },
+  topheight: { type: Number },
+  topspeed: { type: Number },
+  temperature: { type: Number },
 });
 newRocket.save();
 
-const Rocket = mongoose.model('Rocket', RocketSchema);
+const Rocket = mongoose.model("Rocket", RocketSchema);
 module.exports = mongoose.model("rocket", rocketSchema);
