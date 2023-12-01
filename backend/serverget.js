68ed6a9b9fb2e8e5db79de81e0a26d56fbbcc89a
TostoaKula <125472037+TostoaKula@users.noreply.github.com>
2023-12-01 10:21:35 +0100

    comments and cleanup the code on backend

--- backend/serverget.js
+++
@@ -1,9 +0,0 @@
-var requestOptions = {
-  method: "GET",
-  redirect: "follow",
-};
-
-fetch("http://localhost:4000/api/rocket/", requestOptions)
-  .then((response) => response.json())
-  .then((result) => console.log(result))
-  .catch((error) => console.log("error", error));
