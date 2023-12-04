90df0f7741d5f56a807614271b0aafe5935a34ed
TostoaKula <125472037+TostoaKula@users.noreply.github.com>
2023-12-04 12:43:19 +0100

    updated model

--- backend/models/rocket.js
+++ backend/models/rocket.js
@@ -3,10 +3,15 @@ const Schema = mongoose.Schema;
 
 let rocketSchema = new Schema({
   
-  LaunchId: { type: Number },
-  failed: { type: Boolean },
-  topheight: { type: Number },
-  topspeed: { type: Number },
+  launch_id: { type: Number },
+  start_latitude: { type: Number },
+  start_longitude: { type: Number },
+  end_longitude: { type: Number },
+  end_latitude: { type: Number },
+  start_angle: { type: Number },
+  end_angle: { type: Number },
+  //failed: { type: Boolean },
+  altitude: { type: Number },
   temperature: { type: Number },
   created_at: {
     type: Date,
