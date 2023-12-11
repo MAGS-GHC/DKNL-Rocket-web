d7c280378b09df53f9a43e2b4edfd20b23b45449
cbc1000 <127729826+cbc1000@users.noreply.github.com>
2023-12-11 10:04:30 +0100

    Lightmode implemented

--- FrontEnd/js/catalogue.js
+++ FrontEnd/js/catalogue.js
@@ -70,9 +70,7 @@ function createCatalogueHTML(userData){
         ${labels}
     </div>
     <div class="sensorInfo">
-    </div>
-    </div>
-</div>`;
+    </div>`;
 }
 
 function toggleDropdown(index) {
@@ -105,20 +103,19 @@ function toggleDropdown(index) {
     if (currentItem) {
         let formattedDate = formatDate(currentItem.created_at);
     sensorInfo.innerHTML = `
-    <img src="../assets/SVG/arrow.svg" class="arrow sensorInfoArrow" onclick="toggleDropdown(-1)">
-    <p id="sensorName">Launch ID</p>
-    <p id="sensorData">${currentItem.launch_id}</p>
-    <p id="sensorName">Launch date</p>
-    <p id="sensorData">${formattedDate}</p>
-    <p id="sensorName">Height reached</p>
-    <p id="sensorData">${currentItem.altitude}</p>
-    <p id="sensorName">Temperature</p>
-    <p id="sensorData">${currentItem.temperature}</p>
-    <p id="sensorName">Latitude change</p>
-    <p id="sensorData">${currentItem.start_latitude} - ${currentItem.end_latitude}</p>
-    <p id="sensorName">Longitude change</p>
-    <p id="sensorData">${currentItem.start_longitude} - ${currentItem.end_longitude}</p>
-    `;
+        <img src="../assets/SVG/arrow.svg" class="arrow sensorInfoArrow" onclick="toggleDropdown(-1)">
+        <p id="sensorName">Launch ID</p>
+        <p id="sensorData">${currentItem.launch_id}</p>
+        <p id="sensorName">Launch date</p>
+        <p id="sensorData">${formattedDate}</p>
+        <p id="sensorName">Height reached</p>
+        <p id="sensorData">${currentItem.altitude}</p>
+        <p id="sensorName">Temperature</p>
+        <p id="sensorData">${currentItem.temperature}</p>
+        <p id="sensorName">Latitude change</p>
+        <p id="sensorData">${currentItem.start_latitude} - ${currentItem.end_latitude}</p>
+        <p id="sensorName">Longitude change</p>
+        <p id="sensorData">${currentItem.start_longitude} - ${currentItem.end_longitude}</p>`;
     } else {
         console.error(`No data found for index ${index}`);
     }
