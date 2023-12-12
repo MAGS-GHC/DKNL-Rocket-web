31174042e72581ad02d93ef752346abf972832eb
cbc1000 <127729826+cbc1000@users.noreply.github.com>
2023-12-12 10:03:39 +0100

    settings done better

--- FrontEnd/js/lightMode.js
+++ FrontEnd/js/lightMode.js
@@ -7,12 +7,19 @@ function colorThemeFunction() {
     }
 
     const checkbox = document.getElementById('checkbox');
+    const themeToggleText = document.querySelector('.themeChangeText');
     if (checkbox) {
         checkbox.checked = savedCheckboxState;
         checkbox.addEventListener('change', () => {
             document.documentElement.classList.toggle('light-theme');
             localStorage.setItem('theme', document.documentElement.classList.contains('light-theme') ? 'light-theme' : 'dark-theme');
             localStorage.setItem('checkbox', checkbox.checked);
+
+            if (document.documentElement.classList.contains('light-theme')) {
+                themeToggleText.textContent = 'Turn on darkmode';
+            } else {
+                themeToggleText.textContent = 'Turn on lightmode';
+            }
         });
     }
 
